82bc02ed346f2383aad51d5f16f2960c2f5c7be8
alissonover <106097261+alissonover@users.noreply.github.com>
2022-12-13 16:05:28 -0300

    Add files via upload
    First codes on eclipse IDE

---
+++ TestaDouble.java
@@ -0,0 +1,18 @@
+package sintaxeVarFluxo;
+
+public class TestaDouble {
+	public static void main(String[] args) {
+		
+		double salario = 1250.50;
+		System.out.println("O salario e "+salario);
+		
+		double divisao = 3.14/2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5/2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0/2;
+		System.out.println(novaTentativa);
+	}
+}
---
+++ TestaVar.java
@@ -0,0 +1,11 @@
+package sintaxeVarFluxo;
+
+public class TestaVar {
+	public static void main(String[] args) {
+		System.out.println("Ola novo teste");
+		
+		int idade = 29;
+		
+		System.out.println("A idade e: "+idade);
+	}
+}
